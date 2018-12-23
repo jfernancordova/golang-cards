8c23428c2af77a00d47e8d383bb99cf7032b3504
José Fernando Cordova <jfernancordova@gmail.com>
2018-12-23 19:43:13 -0300

    variable declarations

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func main(){
-	
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades" //first initialize the value short declaration
+	card = "Ace of Diamonds" //just assigning
+	fmt.Println(card) 
 }
