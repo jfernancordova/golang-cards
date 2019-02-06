9181a3aba43f92a93908dd8a3733ee41a486043e
José Fernando <jfernancordova@gmail.com>
2019-02-06 20:41:29 -0300

    Declaring Structs

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,14 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName string
 }
 
 func main(){
-	
+	jose := person{ firstName: "José", lastName: "Cordova"}
+	fmt.Println(jose)
+
 }
